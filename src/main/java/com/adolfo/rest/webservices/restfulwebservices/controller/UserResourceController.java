bbda96b3974ad37fe15a180ab3c749208ba924ce
Adolfo <aantars@hotmail.com>
2022-08-19 16:25:14 +0200

    saving new user

--- src/main/java/com/adolfo/rest/webservices/restfulwebservices/controller/UserResourceController.java
+++ src/main/java/com/adolfo/rest/webservices/restfulwebservices/controller/UserResourceController.java
@@ -3,9 +3,7 @@ package com.adolfo.rest.webservices.restfulwebservices.controller;
 import com.adolfo.rest.webservices.restfulwebservices.entity.User;
 import com.adolfo.rest.webservices.restfulwebservices.entity.dao.UserDAOService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,11 @@ public class UserResourceController {
     @GetMapping(path="/users/{id}")
     public User getUser(@PathVariable int id){
         return userDAOService.findOne(id);
+    }
+
+    // save new user
+    @PostMapping(path="/users")
+    public User createUser(@RequestBody User user){
+        return userDAOService.save(user);
     }
 }
