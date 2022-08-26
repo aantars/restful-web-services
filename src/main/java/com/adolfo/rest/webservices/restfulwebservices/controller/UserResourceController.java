85496a05f5279cdd5dce692925edda863a54088d
Adolfo <aantars@hotmail.com>
2022-08-26 14:56:24 +0200

    adding validation, swagger, actuator

--- src/main/java/com/adolfo/rest/webservices/restfulwebservices/controller/UserResourceController.java
+++ src/main/java/com/adolfo/rest/webservices/restfulwebservices/controller/UserResourceController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,7 +44,7 @@ public class UserResourceController {
 
     // save new user
     @PostMapping(path="/users")
-    public ResponseEntity createUser(@RequestBody User user){
+    public ResponseEntity createUser(@Valid @RequestBody User user){
         User savedUser;
         try {
             savedUser = userDAOService.save(user);
--- src/main/java/com/adolfo/rest/webservices/restfulwebservices/entity/User.java
+++ src/main/java/com/adolfo/rest/webservices/restfulwebservices/entity/User.java
@@ -1,11 +1,16 @@
 package com.adolfo.rest.webservices.restfulwebservices.entity;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+    @Size(min=2, message="minimum 2 characters")
     private String name;
+    @Past
     private Date birth;
 
     public User(Integer id, String name, Date birth) {
